28a8c5dd39f9513cbf0d4d97770fc93f3a85b70e
laterpay <laterpay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-01 07:13:59 +0000

    Release v0.9.8
    git-svn-id: https://plugins.svn.wordpress.org/laterpay/tags/v0.9.8@999636 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/backend/partials/navigation.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
+
+<ul class="lp_nav-tabs">
+    <?php $num = 0; ?>
+    <?php foreach ( $menu as $page ): ?>
+        <?php
+            $slug = ! $num ? $plugin_page : $page['url'];
+            if ( $activated === '' ) { // never activated before
+                $slug = $plugin_page;
+            }
+        ?>
+        <li<?php if ( $current_page == $page['url'] || ( ! $num && $current_page == $plugin_page ) ): ?> class="lp_current"<?php endif; ?>>
+            <a href="<?php echo add_query_arg(array('page' => $slug), admin_url('admin.php'));?>" class="lp_d-block"<?php if ( $current_page == $page['url'] || ( ! $num && $current_page == $plugin_page ) ): ?> rel="prefetch"<?php endif; ?>>
+                <?php echo $page['title']; ?>
+            </a>
+        </li>
+        <?php $num++ ?>
+    <?php endforeach; ?>
+</ul>
